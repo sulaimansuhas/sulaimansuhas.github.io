680354bc7f87c7f79ec2ead4ca44dc969a49fe3e
Suhas <sn@Suhass-MacBook-Pro.local>
2023-07-08 22:05:15 +0530

    Indicate which menu item we are on

--- src/components/titlebar/index.tsx
+++ src/components/titlebar/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import styled from "styled-components"
 import { motion } from "framer-motion"
+import { useLocation } from '@reach/router';
 
 import  UnstyledLocalLink  from "../UnstyledLocalLink"
 
@@ -17,16 +18,28 @@ color: #263D42;
 font-family: "Work Sans";
 font-size: 1.3em;
 margin-block-end: 0px;
+font-weight: lighter;
+`
+const SelectedCaption = styled(motion.p)`
+color: #263D42;
+font-family: "Work Sans";
+font-size: 1.3em;
+margin-block-end: 0px;
+font-weight: bolder;
 `
 
+
 export default function TitleBar (): JSX.Element | null {
+  const location = useLocation();
+
+  console.log(location.pathname); // Current page route
 	return(
 		<>
 			<ContainingDiv>
 				<UnstyledLocalLink to="/"><Caption>Home</Caption></UnstyledLocalLink>
-				<UnstyledLocalLink to="/aboutme"><Caption>About</Caption></UnstyledLocalLink>
-				<UnstyledLocalLink to="/resume"><Caption>Resume</Caption></UnstyledLocalLink>
-				<UnstyledLocalLink to="/contact"><Caption>Contact</Caption></UnstyledLocalLink>
+				<UnstyledLocalLink to="/aboutme">{location.pathname === "/aboutme/"?<SelectedCaption>About</SelectedCaption> : <Caption>About</Caption>}</UnstyledLocalLink>
+				<UnstyledLocalLink to="/resume">{location.pathname === "/resume/"?<SelectedCaption>Resume</SelectedCaption> : <Caption>Resume</Caption>}</UnstyledLocalLink>
+				<UnstyledLocalLink to="/contact">{location.pathname === "/contact/"?<SelectedCaption>Contact</SelectedCaption> : <Caption>Contact</Caption>}</UnstyledLocalLink>
 			</ContainingDiv>
 			</>
 		);
